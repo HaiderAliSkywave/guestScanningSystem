73d465d396987b5b271ced225021c42ebc02c525
HaiderAliSkywave <haider.ali@skywavetechnology.com>
2025-02-15 04:24:19 +0500

    users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,103 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('admin@12345'),
             'role' => 'admin',
         ]);
+        
+        User::factory()->create([
+            'name' => 'Searcher',
+            'email' => 'searcher1@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'searcher',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Searcher',
+            'email' => 'searcher2@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'searcher',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Searcher',
+            'email' => 'searcher3@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'searcher',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Searcher',
+            'email' => 'searcher4@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'searcher',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender1@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender2@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender3@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender4@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender5@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender6@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender7@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender8@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender9@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'Attender',
+            'email' => 'attender10@gss.com',
+            'password' => Hash::make('password'),
+            'role' => 'attender',
+        ]);
     }
 }
